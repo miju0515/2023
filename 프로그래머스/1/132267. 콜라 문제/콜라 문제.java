e80536cfaf239ee874568e337d12f77aa9ccca61
KIM Miju <76895039+miju0515@users.noreply.github.com>
2024-01-29 13:29:46 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        int left = n;
+        while(left>=a){
+            answer += (left/a)*b;
+            left = (left%a)+(left/a)*b;
+        }
+        return answer;
+    }
+}
